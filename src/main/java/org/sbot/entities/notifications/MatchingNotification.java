b28f1160c42a6d2cf543304e43df52bbba3da790
skirb <nomail@nomail.com>
2024-03-21 15:09:17 +0100

    MatchingNotification

---
+++ src/main/java/org/sbot/entities/notifications/MatchingNotification.java
@@ -0,0 +1,116 @@
+package org.sbot.entities.notifications;
+
+import net.dv8tion.jda.api.entities.Role;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.sbot.entities.FieldParser;
+import org.sbot.entities.Message;
+import org.sbot.entities.alerts.Alert;
+import org.sbot.entities.chart.DatedPrice;
+import org.sbot.services.MatchingService.MatchingAlert.MatchingStatus;
+import org.sbot.services.context.Context;
+import org.sbot.services.discord.Discord;
+
+import java.awt.*;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.*;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.function.Predicate.not;
+import static org.sbot.entities.FieldParser.Type.DECIMAL;
+import static org.sbot.entities.FieldParser.Type.ZONED_DATE_TIME;
+import static org.sbot.entities.alerts.Alert.Type.remainder;
+import static org.sbot.entities.alerts.Alert.isPrivate;
+import static org.sbot.entities.notifications.MatchingNotification.Field.*;
+import static org.sbot.entities.notifications.Notification.NotificationStatus.NEW;
+import static org.sbot.entities.notifications.Notification.RecipientType.DISCORD_SERVER;
+import static org.sbot.entities.notifications.Notification.RecipientType.DISCORD_USER;
+import static org.sbot.services.MatchingService.MatchingAlert.MatchingStatus.MARGIN;
+import static org.sbot.services.MatchingService.MatchingAlert.MatchingStatus.MATCHED;
+
+public final class MatchingNotification extends Notification {
+
+    public enum Field implements FieldParser {
+        LAST_CLOSE(DECIMAL),
+        LAST_CLOSE_TIME(ZONED_DATE_TIME),
+        LAST_TRIGGER(ZONED_DATE_TIME);
+
+        private final Type type;
+
+        Field(@NotNull Type type) {
+            this.type = requireNonNull(type);
+        }
+
+        @NotNull
+        @Override
+        public Object parse(@NotNull String value) {
+            return type.parse(value);
+        }
+    }
+
+    public static final Color MATCHED_COLOR = Color.green;
+    public static final Color MARGIN_COLOR = Color.orange;
+
+    private MatchingNotification(long id, @NotNull ZonedDateTime creationDate, @NotNull NotificationStatus status, @NotNull MatchingStatus matchingStatus, @NotNull RecipientType recipientType, @NotNull String recipientId, @NotNull Locale locale, @NotNull Map<FieldParser, Object> fields) {
+        super(id, creationDate, status, Optional.of(matchingStatus).filter(not(MatchingStatus::notMatching)).orElseThrow(() -> new IllegalArgumentException("Not in matching status, recipient :" + recipientId + ", fields : " + fields)).isMatched() ?
+                NotificationType.MATCHED : NotificationType.MARGIN, recipientType, recipientId, locale, fields);
+    }
+
+    public MatchingNotification(long id, @NotNull ZonedDateTime creationDate, @NotNull NotificationStatus status, @NotNull MatchingStatus matchingStatus, @NotNull RecipientType recipientType, @NotNull String recipientId, @NotNull Locale locale, @NotNull String fields) {
+        this(id, creationDate, status, matchingStatus, recipientType, recipientId, locale, fieldsOf(fields, Field.values(), true));
+    }
+
+    public static MatchingNotification of(@NotNull ZonedDateTime now, @NotNull Locale locale, @NotNull MatchingStatus matchingStatus, @NotNull Alert alert, @Nullable DatedPrice previousClose) {
+        Map<FieldParser, Object> fields = new HashMap<>();
+        if(null != previousClose) {
+            fields.put(LAST_CLOSE, previousClose.price());
+            fields.put(LAST_CLOSE_TIME, previousClose.dateTime());
+        }
+        fields.put(LAST_TRIGGER, alert.lastTrigger);
+        fields.putAll(alert.fieldsMap());
+        var recipientType = isPrivate(alert.serverId) ? DISCORD_USER : DISCORD_SERVER;
+        var recipientId = DISCORD_USER.equals(recipientType) ? alert.userId : alert.serverId;
+        return new MatchingNotification(NEW_NOTIFICATION_ID, now, NEW, matchingStatus, recipientType, String.valueOf(recipientId), locale, fields);
+    }
+
+    @Override
+    @NotNull
+    protected MatchingNotification build(long id, @NotNull ZonedDateTime creationDate, @NotNull NotificationStatus status, @NotNull NotificationType type, @NotNull Notification.RecipientType recipientType, @NotNull String recipientId, @NotNull Locale locale, @NotNull Map<FieldParser, Object> fields) {
+        return new MatchingNotification(id, creationDate, status, NotificationType.MATCHED == type ? MATCHED : MARGIN, recipientType, recipientId, locale, fields);
+    }
+
+    @Override
+    @NotNull
+    public CharSequence serializedFields() {
+        return serializedFields(Field.values(), true);
+    }
+
+    @Override
+    @NotNull
+    public Message asMessage(@NotNull Context context) {
+        Alert alert = Alert.of(fields);
+        var matchingType = NotificationType.MATCHED == type ? MATCHED : MARGIN;
+        var lastClose = (BigDecimal) fields.get(LAST_CLOSE);
+        var lastCloseTime = (ZonedDateTime) fields.get(LAST_CLOSE_TIME);
+        var previousClose = null != lastClose && null != lastCloseTime ? new DatedPrice(lastClose, lastCloseTime) : null;
+        var embed = alert.asMessage(matchingType, previousClose, (ZonedDateTime) fields.get(LAST_TRIGGER))
+                .setTitle(raiseTitle(alert, matchingType))
+                .setColor(NotificationType.MATCHED == type ? MATCHED_COLOR : MARGIN_COLOR);
+
+        if(DISCORD_SERVER.equals(recipientType)) {
+            List<String> roles = context.discord().guildServer(alert.serverId).flatMap(Discord::spotBotRole).map(Role::getId).stream().toList();
+            var users = List.of(String.valueOf(alert.userId));
+            return Message.of(List.of(embed), roles, users);
+        }
+        return Message.of(embed);
+    }
+
+    static String raiseTitle(@NotNull Alert alert, @NotNull MatchingStatus matchingStatus) {
+        boolean remainderAlert = remainder == alert.type;
+        String title = "!!! " + (matchingStatus.isMargin() ? "MARGIN " : "") + alert.type.titleName;
+        title += (remainderAlert ?  " !!!" : " ALERT !!!") + " - [" + alert.pair + "]";
+        return remainderAlert ? title : title + ' ' + alert.message;
+    }
+}
---
+++ src/test/java/org/sbot/entities/notifications/MatchingNotificationTest.java
@@ -0,0 +1,162 @@
+package org.sbot.entities.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.sbot.entities.alerts.Alert;
+import org.sbot.entities.chart.DatedPrice;
+import org.sbot.entities.notifications.Notification.NotificationType;
+import org.sbot.services.MatchingService.MatchingAlert.MatchingStatus;
+import org.sbot.services.context.Context;
+import org.sbot.services.discord.Discord;
+import org.sbot.utils.DatesTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static java.math.BigDecimal.ONE;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.sbot.entities.FieldParser.format;
+import static org.sbot.entities.User.DEFAULT_LOCALE;
+import static org.sbot.entities.alerts.Alert.PRIVATE_MESSAGES;
+import static org.sbot.entities.alerts.Alert.Type.*;
+import static org.sbot.entities.alerts.AlertTest.*;
+import static org.sbot.entities.notifications.MatchingNotification.Field.*;
+import static org.sbot.entities.notifications.MatchingNotification.MARGIN_COLOR;
+import static org.sbot.entities.notifications.MatchingNotification.MATCHED_COLOR;
+import static org.sbot.entities.notifications.Notification.NEW_NOTIFICATION_ID;
+import static org.sbot.entities.notifications.Notification.NotificationStatus.NEW;
+import static org.sbot.entities.notifications.Notification.NotificationStatus.SENDING;
+import static org.sbot.entities.notifications.Notification.RecipientType.DISCORD_SERVER;
+import static org.sbot.entities.notifications.Notification.RecipientType.DISCORD_USER;
+import static org.sbot.entities.notifications.Notification.SOH;
+import static org.sbot.services.MatchingService.MatchingAlert.MatchingStatus.MARGIN;
+import static org.sbot.services.MatchingService.MatchingAlert.MatchingStatus.MATCHED;
+import static org.sbot.utils.ArgumentValidator.requireOneItem;
+
+class MatchingNotificationTest {
+
+    @Test
+    void of() {
+        var now = DatesTest.nowUtc();
+        assertThrows(NullPointerException.class, () -> MatchingNotification.of(null, DEFAULT_LOCALE, MatchingStatus.MARGIN, createTestAlert(), new DatedPrice(ONE, DatesTest.nowUtc())));
+        assertThrows(NullPointerException.class, () -> MatchingNotification.of(now, null, MatchingStatus.MARGIN, createTestAlert(), new DatedPrice(ONE, DatesTest.nowUtc())));
+        assertThrows(NullPointerException.class, () -> MatchingNotification.of(now, DEFAULT_LOCALE, null, createTestAlert(), new DatedPrice(ONE, DatesTest.nowUtc())));
+        assertThrows(NullPointerException.class, () -> MatchingNotification.of(now, DEFAULT_LOCALE, MatchingStatus.MARGIN, null, new DatedPrice(ONE, DatesTest.nowUtc())));
+
+        var alert = createTestAlert();
+        var notification = MatchingNotification.of(now, DEFAULT_LOCALE, MatchingStatus.MARGIN, alert, new DatedPrice(ONE, now));
+        assertEquals(NEW_NOTIFICATION_ID, notification.id);
+        assertEquals(now, notification.creationDate);
+        assertEquals(NEW, notification.status);
+        assertEquals(String.valueOf(alert.serverId), notification.recipientId);
+        assertEquals(DISCORD_SERVER, notification.recipientType);
+        assertEquals(DEFAULT_LOCALE, notification.locale);
+
+        assertEquals(ONE, notification.fields.get(LAST_CLOSE));
+        assertEquals(now, notification.fields.get(LAST_CLOSE_TIME));
+        assertEquals(alert.lastTrigger, notification.fields.get(LAST_TRIGGER));
+        assertTrue(alert.fieldsMap().entrySet().stream().allMatch(e -> notification.fields.get(e.getKey()).equals(e.getValue())));
+
+        alert = alert.withServerId(PRIVATE_MESSAGES);
+        var notification2 = MatchingNotification.of(now, DEFAULT_LOCALE, MatchingStatus.MARGIN, alert, new DatedPrice(ONE, now));
+        assertEquals(NEW_NOTIFICATION_ID, notification2.id);
+        assertEquals(now, notification2.creationDate);
+        assertEquals(NEW, notification2.status);
+        assertEquals(String.valueOf(alert.userId), notification2.recipientId);
+        assertEquals(DISCORD_USER, notification2.recipientType);
+        assertEquals(DEFAULT_LOCALE, notification2.locale);
+        assertTrue(alert.fieldsMap().entrySet().stream().allMatch(e -> notification2.fields.get(e.getKey()).equals(e.getValue())));
+    }
+
+    @Test
+    void build() {
+        var notification = MatchingNotification.of(DatesTest.nowUtc(), DEFAULT_LOCALE, MatchingStatus.MARGIN, createTestAlert(), new DatedPrice(ONE, DatesTest.nowUtc()));
+        assertEquals(notification, notification.withStatus(SENDING));
+        assertEquals(NotificationType.MARGIN, notification.type);
+        notification = MatchingNotification.of(DatesTest.nowUtc(), DEFAULT_LOCALE, MATCHED, createTestAlert(), new DatedPrice(ONE, DatesTest.nowUtc()));
+        assertEquals(NotificationType.MATCHED, notification.type);
+    }
+
+    @Test
+    void serializedFields() {
+        var notification = MatchingNotification.of(DatesTest.nowUtc(), DEFAULT_LOCALE, MatchingStatus.MARGIN, createTestAlert(), new DatedPrice(ONE, DatesTest.nowUtc()));
+        var fields = format(notification.fields.get(LAST_CLOSE)) + SOH;
+        fields += format(notification.fields.get(LAST_CLOSE_TIME)) + SOH;
+        fields += format(notification.fields.get(LAST_TRIGGER)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.ID)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.TYPE)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.USER_ID)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.SERVER_ID)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.CREATION_DATE)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.LISTENING_DATE)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.LAST_TRIGGER)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.EXCHANGE)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.PAIR)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.MESSAGE)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.FROM_PRICE)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.TO_PRICE)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.FROM_DATE)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.TO_DATE)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.MARGIN)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.REPEAT)) + SOH;
+        fields += format(notification.fields.get(Alert.Field.SNOOZE));
+        assertEquals(fields, notification.serializedFields().toString());
+    }
+
+    @Test
+    void asMessage() {
+        var alert = createTestAlert();
+        Context context = mock();
+        Discord discord = mock();
+        when(context.discord()).thenReturn(discord);
+        when(discord.guildServer(alert.serverId)).thenReturn(Optional.empty());
+
+        var notification = MatchingNotification.of(DatesTest.nowUtc(), DEFAULT_LOCALE, MatchingStatus.MARGIN, alert, new DatedPrice(ONE, DatesTest.nowUtc()));
+        var message = notification.asMessage(context);
+        var embed = requireOneItem(message.embeds()).build();
+        assertEquals("!!! MARGIN Range ALERT !!! - [BTC/USD] test message", embed.getTitle());
+        assertTrue(embed.getDescription().startsWith("<@1234>\nYour range set on binance BTC/USD reached **margin** threshold"));
+        assertEquals(MARGIN_COLOR, embed.getColor());
+        verify(context).discord();
+        verify(discord).guildServer(alert.serverId);
+        assertEquals(List.of(String.valueOf(alert.userId)), message.mentionUsers());
+        assertEquals(List.of(), message.mentionRoles());
+
+        notification = MatchingNotification.of(DatesTest.nowUtc(), DEFAULT_LOCALE, MATCHED, alert.withServerId(PRIVATE_MESSAGES), new DatedPrice(ONE, DatesTest.nowUtc()));
+        message = notification.asMessage(context);
+        embed = requireOneItem(message.embeds()).build();
+        assertEquals("!!! Range ALERT !!! - [BTC/USD] test message", embed.getTitle());
+        assertTrue(embed.getDescription().startsWith("<@1234>\nYour range set on binance BTC/USD was **tested !**"));
+        assertEquals(MATCHED_COLOR, embed.getColor());
+        verify(context).discord();
+        verify(discord).guildServer(alert.serverId);
+        assertNull(message.mentionUsers());
+        assertNull(message.mentionRoles());
+    }
+
+    @Test
+    void raiseTitle() {
+        var alert = createTestAlert();
+        assertEquals("!!! MARGIN Range ALERT !!! - [BTC/USD] test message", MatchingNotification.raiseTitle(alert, MARGIN));
+        assertEquals("!!! Range ALERT !!! - [BTC/USD] test message", MatchingNotification.raiseTitle(alert, MATCHED));
+        var longMess = "1234567890".repeat(21);
+        alert = alert.withMessage(longMess);
+        assertThrows(IllegalArgumentException.class, () -> createTestAlert().withMessage(longMess + "1"));
+
+        assertEquals("!!! MARGIN Range ALERT !!! - [BTC/USD] " + longMess, MatchingNotification.raiseTitle(alert, MARGIN));
+        assertEquals("!!! Range ALERT !!! - [BTC/USD] " + longMess, MatchingNotification.raiseTitle(alert, MATCHED));
+
+        assertThrows(IllegalArgumentException.class, () -> createTestAlertWithUserIdAndPairType(TEST_USER_ID, "123456/12346", range));
+        alert = createTestAlertWithUserIdAndPairType(TEST_USER_ID, "12345/12346", range).withMessage(longMess);
+        assertEquals("!!! MARGIN Range ALERT !!! - [12345/12346] " + longMess, MatchingNotification.raiseTitle(alert, MARGIN));
+        assertEquals("!!! Range ALERT !!! - [12345/12346] " + longMess, MatchingNotification.raiseTitle(alert, MATCHED));
+
+        alert = createTestAlertWithUserIdAndPairType(TEST_USER_ID, "12345/12346", trend).withMessage(longMess);
+        assertEquals("!!! MARGIN Trend ALERT !!! - [12345/12346] " + longMess, MatchingNotification.raiseTitle(alert, MARGIN));
+        assertEquals("!!! Trend ALERT !!! - [12345/12346] " + longMess, MatchingNotification.raiseTitle(alert, MATCHED));
+
+        alert = createTestAlertWithUserIdAndPairType(TEST_USER_ID, "12345/12346", remainder).withMessage(longMess);
+        assertEquals("!!! MARGIN Remainder !!! - [12345/12346]", MatchingNotification.raiseTitle(alert, MARGIN));
+        assertEquals("!!! Remainder !!! - [12345/12346]", MatchingNotification.raiseTitle(alert, MATCHED));
+    }
+}
