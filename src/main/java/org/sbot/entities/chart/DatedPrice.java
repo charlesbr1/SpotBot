f9e095d14fd3fb3f08b6f0d8cf8e3674eabcd31c
skirb <nomail@nomail.com>
2024-04-22 14:48:33 +0200

    DatedPriceTest

--- src/main/java/org/sbot/entities/chart/DatedPrice.java
+++ src/main/java/org/sbot/entities/chart/DatedPrice.java
@@ -1,15 +1,14 @@
 package org.sbot.entities.chart;
 
 import org.jetbrains.annotations.NotNull;
-
-import java.math.BigDecimal;
-import java.time.ZonedDateTime;
+import org.sbot.utils.MutableDecimal;
 
 import static java.util.Objects.requireNonNull;
+import static org.sbot.utils.ArgumentValidator.requireEpoch;
 
-public record DatedPrice(@NotNull BigDecimal price, @NotNull ZonedDateTime dateTime) {
+public record DatedPrice(@NotNull MutableDecimal price, long dateTime) {
     public DatedPrice {
         requireNonNull(price);
-        requireNonNull(dateTime);
+        requireEpoch(dateTime);
     }
 }
--- src/test/java/org/sbot/entities/chart/DatedPriceTest.java
+++ src/test/java/org/sbot/entities/chart/DatedPriceTest.java
@@ -1,23 +1,22 @@
 package org.sbot.entities.chart;
 
 import org.junit.jupiter.api.Test;
-import org.sbot.utils.Dates;
 import org.sbot.utils.DatesTest;
 
-import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.sbot.entities.alerts.Alert.NO_DATE;
+import static org.sbot.utils.MutableDecimalTest.ONE;
 
 class DatedPriceTest {
 
     @Test
     void constructorCheck() {
         var now = DatesTest.nowUtc();
-        assertDoesNotThrow(() -> new DatedPrice(BigDecimal.ONE, now));
-        assertThrows(NullPointerException.class, () -> new DatedPrice(null, null));
+        assertDoesNotThrow(() -> new DatedPrice(ONE, now));
         assertThrows(NullPointerException.class, () -> new DatedPrice(null, now));
-        assertThrows(NullPointerException.class, () -> new DatedPrice(BigDecimal.ONE, null));
-        assertEquals(BigDecimal.ONE, new DatedPrice(BigDecimal.ONE, now).price());
-        assertEquals(now, new DatedPrice(BigDecimal.ONE, now).dateTime());
+        assertThrows(IllegalArgumentException.class, () -> new DatedPrice(ONE, NO_DATE));
+        assertEquals(ONE, new DatedPrice(ONE, now).price());
+        assertEquals(now, new DatedPrice(ONE, now).dateTime());
     }
 }
